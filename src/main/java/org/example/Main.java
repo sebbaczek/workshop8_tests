a6d2dfc1aab288dc0845cda03557ffc48014cea0
DESKTOP-BFV5RNS\Sebastian <sebastian451@interia.pl>
2023-01-05 10:48:29 +0100

    changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+package org.example;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+        public static void main(String[] args) {
+                System.out.println("Hello world!");
+        }
 }
